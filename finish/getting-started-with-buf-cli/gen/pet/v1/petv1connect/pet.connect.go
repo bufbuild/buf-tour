2ef08703c9f60ddfe3ec13110142726546fb45fe
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-02-10 11:15:25 -0500

    update cli finish

--- finish/getting-started-with-buf-cli/gen/pet/v1/petv1connect/pet.connect.go
+++ finish/getting-started-with-buf-cli/gen/pet/v1/petv1connect/pet.connect.go
@@ -7,7 +7,7 @@ package petv1connect
 import (
 	context "context"
 	errors "errors"
-	v1 "github.com/bufbuild/buf-tour/finish/getting-started-with-buf-cli/gen/proto/pet/v1"
+	v1 "github.com/bufbuild/buf-tour/gen/pet/v1"
 	connect_go "github.com/bufbuild/connect-go"
 	http "net/http"
 	strings "strings"
